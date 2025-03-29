0db7828d260b466fa6a9f2dcf725f2d2bdc0ae7c
Simone Tesini <simozilla006@gmail.com>
2025-03-30 00:06:04 +0100

    init

---
+++ minecraft/kubejs/server_scripts/example.js
@@ -0,0 +1,14 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+ServerEvents.recipes(event => {
+	// You can replace `event` with any name you like, as
+	// long as you change it inside the callback too!
+
+	// This part, inside the curly braces, is the callback.
+	// You can modify as many recipes as you like in here,
+	// without needing to use ServerEvents.recipes() again.
+
+	console.log('Hello! The recipe event has fired!')
+})
