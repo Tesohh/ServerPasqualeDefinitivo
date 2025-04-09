0b4c9e457811843760acdb4fd16c0f62d400f4fd
Simone Tesini <simozilla006@gmail.com>
2025-04-09 21:06:08 +0200

    change electron tube id

--- minecraft/kubejs/server_scripts/hulls/train_hulls.js
+++ minecraft/kubejs/server_scripts/hulls/train_hulls.js
@@ -4,6 +4,6 @@ ServerEvents.recipes(event => {
 	let hull = "kubejs:train_machine_hull"
 	hull_crafting(event, hull, "create:track_station", ["compass"], "create:crafting/kinetics/track_station")
 	hull_crafting(event, hull, "2x create:track_observer", ["observer"], "create:crafting/kinetics/track_observer")
-	hull_crafting(event, hull, "4x create:track_signal", ["electron_tube"], "create:crafting/kinetics/track_signal")
+	hull_crafting(event, hull, "4x create:track_signal", ["create:electron_tube"], "create:crafting/kinetics/track_signal")
 	hull_crafting(event, hull, "create:controls", ["lever", "lever"], "create:crafting/kinetics/controls")
 })
