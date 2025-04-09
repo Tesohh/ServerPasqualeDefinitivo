c2f9a8a26f59aff028d58868b121b0195ecb7529
Simone Tesini <simozilla006@gmail.com>
2025-04-09 21:05:53 +0200

    add train hulls

--- minecraft/kubejs/server_scripts/hulls/train_hulls.js
+++ minecraft/kubejs/server_scripts/hulls/train_hulls.js
@@ -1,5 +1,9 @@
 // priority: 0
 
 ServerEvents.recipes(event => {
-	let hull = "kubejs:copper_machine_hull"
+	let hull = "kubejs:train_machine_hull"
+	hull_crafting(event, hull, "create:track_station", ["compass"], "create:crafting/kinetics/track_station")
+	hull_crafting(event, hull, "2x create:track_observer", ["observer"], "create:crafting/kinetics/track_observer")
+	hull_crafting(event, hull, "4x create:track_signal", ["electron_tube"], "create:crafting/kinetics/track_signal")
+	hull_crafting(event, hull, "create:controls", ["lever", "lever"], "create:crafting/kinetics/controls")
 })
