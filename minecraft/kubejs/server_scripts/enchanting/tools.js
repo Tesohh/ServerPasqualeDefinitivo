addef711bcf6a4cf4adc8c12067fb92e49f21efc
Simone Tesini <simozilla006@gmail.com>
2025-04-10 16:44:02 +0200

    nerf enchantment crafting

--- minecraft/kubejs/server_scripts/enchanting/tools.js
+++ minecraft/kubejs/server_scripts/enchanting/tools.js
@@ -1,7 +1,8 @@
 // priority: 0
 
 ServerEvents.recipes(event => {
-	enchant_spouting(event, "efficiency", 5, "artifacts:onion_ring", true)
+	enchant_spouting(event, "efficiency", 4, "artifacts:onion_ring")
+	enchant_spouting(event, "efficiency", 5, "artifacts:digging_claws", true)
 	enchant_crafting(event, "silk_touch", 1, "brush")
 	enchant_crafting(event, "fortune", 3, "artifacts:lucky_scarf", true)
 })
